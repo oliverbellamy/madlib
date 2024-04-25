b87c10c2bbf8d5f6ff165886e2965996867c3102
oliver <114929344+oliverbellamy@users.noreply.github.com>
2024-04-25 15:48:22 -0300

    realizacion de programa
    se realizo programa de cambiar palabras en la historia

---
+++ historias-locas.py
@@ -0,0 +1,16 @@
+# organizacion = "freeCodeCamp"
+
+# print("Aprende a programar con " + organizacion)
+# print("Aprende a programar con {}".format(organizacion))
+# print(f"Aprende a programar con {organizacion}")
+
+#Historias locas
+
+adj = input("Adjetivo: ")
+verbo1 = input("Verbo: ")
+verbo2 = input("Verbo: ")
+sustantivo_plural = input("Sustantivo (plural): ")
+
+madlib = f"Programar es tan {adj}! Siempre me emociona porque me encanta {verbo1} problemas. Aprende a {verbo2} con freeCodeCamp y alcanza tus {sustantivo_plural}"
+
+print(madlib)
