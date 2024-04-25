1f4817482acea94acd8ad787f5ffdcc80871dbc8
oliver <114929344+oliverbellamy@users.noreply.github.com>
2024-04-25 15:49:19 -0300

    Update historias-locas.py
    se modifico

--- historias-locas.py
+++ historias-locas.py
@@ -1,4 +1,4 @@
-# organizacion = "freeCodeCamp"
+# organizacion = "OliverCodeCamp"
 
 # print("Aprende a programar con " + organizacion)
 # print("Aprende a programar con {}".format(organizacion))
@@ -11,6 +11,6 @@ verbo1 = input("Verbo: ")
 verbo2 = input("Verbo: ")
 sustantivo_plural = input("Sustantivo (plural): ")
 
-madlib = f"Programar es tan {adj}! Siempre me emociona porque me encanta {verbo1} problemas. Aprende a {verbo2} con freeCodeCamp y alcanza tus {sustantivo_plural}"
+madlib = f"Programar es tan {adj}! Siempre me emociona porque me encanta {verbo1} problemas. Aprende a {verbo2} con OliverCodeCamp y alcanza tus {sustantivo_plural}"
 
 print(madlib)
